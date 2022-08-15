211836363dd97cd31ab13096eb025a2bdaa1b165
Pranav Rustagi <57594891+Pranav-Rustagi@users.noreply.github.com>
2022-08-15 12:45:34 +0530

    Time: 10 ms (78.86%), Space: 5.9 MB (86.74%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int getNum(char ch) {
+        switch(ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+    
+    int getPrevChar(char ch) {
+        switch(ch) {
+            case 'V': return 'I';
+            case 'X': return 'I';
+            case 'L': return 'X';
+            case 'C': return 'X';
+            case 'D': return 'C';
+            case 'M': return 'C';
+        }
+        return '\0';
+    }
+    
+    int romanToInt(string s) {
+        int res = 0;
+        
+        while(!s.empty()) {
+            char prev = getPrevChar(s.back());
+            res += getNum(s.back());
+            s.pop_back();
+            while(!s.empty() && s.back() == prev) {
+                s.pop_back();
+                res -= getNum(prev);
+            }
+        }
+        
+        return res;
+    }
+};

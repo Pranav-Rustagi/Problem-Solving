36bfc45a57a16c8c6967e44f6fb9d16fa11a64f6
Pranav Rustagi <57594891+Pranav-Rustagi@users.noreply.github.com>
2022-08-24 11:51:42 +0530

    Time: 27 ms (73.41%), Space: 6 MB (29.00%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -3,7 +3,6 @@ public:
     bool isPowerOfThree(int n) {
         if(n < 1)
             return false;
-        double f = log10(n) / log10(3);
-        return (f == ceil(f));
+        return int(pow(3, floor(log(INT_MAX) / log(3)))) % n == 0;
     }
 };

9e2d59c119aaa29eab207cce7cbf5324a034349f
Pranav Rustagi <57594891+Pranav-Rustagi@users.noreply.github.com>
2022-09-03 11:51:05 +0530

    Time: 3 ms (74.35%), Space: 8.1 MB (63.78%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -2,24 +2,31 @@ class Solution {
 public:
     vector<int> r;
     
-    void solve(int n, int k, int acc) {
-        if(to_string(acc).size() == n) {
+    void solve(int n, int& k, int acc) {
+        if(n == 0) {
             r.push_back(acc);
         }
         else {
             int dig = acc % 10;
             if(dig + k <= 9)
-                solve(n, k, acc * 10 + dig + k);
-            if(k && dig - k >= 0)
-                solve(n, k, acc * 10 + dig - k);
+                solve(n - 1, k, acc * 10 + dig + k);
+            if(dig - k >= 0)
+                solve(n - 1, k, acc * 10 + dig - k);
         }
     }
     
     vector<int> numsSameConsecDiff(int n, int k) {
         for(int i = 1 ; i <= 9 ; ++i) {
-            solve(n, k, i);
+            if(k) {
+                solve(n - 1, k, i);
+            }
+            else {
+                int dup = n, acc = 0;
+                while(dup--)
+                    acc = acc * 10 + i;
+                r.push_back(acc);
+            }
         }
-        
         return r;
     }
 };

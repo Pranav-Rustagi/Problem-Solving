1c5e7eb996b6638cc904473430ef517173a7f9bf
Pranav Rustagi <57594891+Pranav-Rustagi@users.noreply.github.com>
2022-10-09 01:37:27 +0530

    Time: 571 ms (29.89%), Space: 16.4 MB (48.63%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -19,9 +19,6 @@ public:
                 else
                     ++p1;
             }
-            
-            while(i + 1 < nums.size() && nums[i] == nums[i + 1])
-                ++i;
         }
         
         return res;

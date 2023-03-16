94d9c93201098eca261e9f38f9c2f4eadb713c1a
Pranav Rustagi <57594891+Pranav-Rustagi@users.noreply.github.com>
2023-03-17 01:54:58 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -4,42 +4,22 @@ using namespace std;
 
 
 // } Driver Code Ends
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define pbdsTree tree<long long, null_type, less_equal<long long>, rb_tree_tag, tree_order_statistics_node_update>
+
 class Solution{
   public:
-    // arr[]: Input Array
-    // N : Size of the Array arr[]
-    // Function to count inversions in the array.
-    void merge(long long arr[], long long start, long long end, long long& count) {
-        if(start >= end)
-            return;
-        long long mid = (start + end) >> 1;
-        merge(arr, start, mid, count);
-        merge(arr, mid + 1, end, count);
-        vector<long long> temp(end - start + 1);
-        long long iter1 = start, iter2 = mid + 1, i = 0;
-        while(iter1 <= mid && iter2 <= end) {
-            if(arr[iter1] <= arr[iter2]) {
-                temp[i++] = arr[iter1++];
-            } else {
-                count += (mid - iter1 + 1);
-                temp[i++] = arr[iter2++];
-            }
-        }
-        
-        while(iter1 <= mid)
-            temp[i++] = arr[iter1++];
-        while(iter2 <= end)
-            temp[i++] = arr[iter2++];
-        i = 0;
-        while(i < temp.size()) {
-            arr[start + i] = temp[i];
-            i++;
-        }
-    }
-    
     long long int inversionCount(long long arr[], long long N) {
         long long count = 0;
-        merge(arr, 0, N - 1, count);
+        pbdsTree tree;
+        while(N--) {
+            tree.insert(arr[N]);
+            count += tree.order_of_key(arr[N]);
+        }
         return count;
     }
 

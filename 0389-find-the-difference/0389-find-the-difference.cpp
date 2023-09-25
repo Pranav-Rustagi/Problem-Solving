4fec421a06ee5d604fc71ff2a5a5e72d986b4bff
Pranav Rustagi <57594891+Pranav-Rustagi@users.noreply.github.com>
2023-09-25 07:39:01 +0530

    Time: 0 ms (100%), Space: 7 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         int arr[26] = {0};
+
         for(char ch : s)
-            arr[ch - 'a']++;
+            ++arr[ch - 'a'];
+
         for(char ch : t)
-            if(arr[ch - 'a']-- == 0)
+            if(--arr[ch - 'a'] == -1)
                 return ch;
+
         return '\0';
     }
 };

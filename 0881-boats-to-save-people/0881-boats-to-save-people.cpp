55aeb8628e356853d664b75907d8a9adb7125e44
Pranav Rustagi <57594891+Pranav-Rustagi@users.noreply.github.com>
2024-05-04 19:45:59 +0530

    Time: 61 ms (35.69%), Space: 45.5 MB (8.97%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int i = 0, j = people.size() - 1, count = 0;
+        
+        while(i <= j) {
+            if(people[i] + people[j] <= limit)  i++;
+            count++;
+            j--;
+        }
+        
+        return count;
+    }
+};

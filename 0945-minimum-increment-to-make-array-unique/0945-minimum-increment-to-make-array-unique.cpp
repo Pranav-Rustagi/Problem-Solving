4836912f1f8fe1841d434c6efe7112691548c271
Pranav Rustagi <57594891+Pranav-Rustagi@users.noreply.github.com>
2024-06-14 21:31:37 +0530

    Time: 122 ms (52.95%), Space: 69.2 MB (50.39%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        bool hash[200001] = {false};
         sort(nums.begin(), nums.end());
         
-        int ind = nums[0], res = 0;
+        int ind = 0, res = 0;
         for(int num : nums) {
             ind = max(ind, num);
-            while(hash[ind]) {
-                ind++;
-            }
-            
-            hash[ind] = true;
             res += ind++ - num;
         }
         
